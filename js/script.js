376d623e3b6840ad32cd5dbc5307df7449e2cf75
ramoshe <rsherard@mac.com>
2021-06-25 13:13:46 -0400

    cleanup etc
    cleaned up some things, submit handler is still not working right

--- js/script.js
+++ js/script.js
@@ -25,12 +25,11 @@ window.addEventListener ('load', () => {
     shirtDesign.addEventListener('change', () => {
         shirtColor.disabled = false;
         const colors = shirtColor.children;
-        
         /**
          * This function updates the option elements in the "Color" select menu based on design choice
          * @param {string} design - the chosen option from the "Design" dropdown
          */
-        function colorsList(design) {
+        function colorOptions(design) {
             let firstLoop = true;
             for (let i=0; i<colors.length; i++) {
                 const theme = colors[i].dataset.theme;
@@ -46,17 +45,27 @@ window.addEventListener ('load', () => {
                 }
             }
         }
-
         if (shirtDesign.value == 'heart js') {
-            colorsList('heart js')
+            colorOptions('heart js')
         }
         if (shirtDesign.value == 'js puns') {
-            colorsList('js puns');
+            colorOptions('js puns');
         }
     });
 
-// Update price in "Register for Activities" section
+// Accessibility - make focus states of "Activities" more obvious
     const activities = document.querySelector('#activities');
+    const checkboxes = activities.querySelectorAll('[type="checkbox"]');
+    for (let i=0; i<checkboxes.length; i++) {
+        checkboxes[i].addEventListener('focus', (e) => {
+            checkboxes[i].parentElement.className = 'focus';
+        })
+        checkboxes[i].addEventListener('blur', (e) => {
+            checkboxes[i].parentElement.removeAttribute('class', 'focus')
+        })
+    }
+
+// Update price in "Register for Activities" section
     let totalDisplay = document.querySelector('#activities-cost');
     let cost = 0;
 
@@ -97,30 +106,24 @@ window.addEventListener ('load', () => {
         }
     });
 
-// Form validation helper functions
+// Elements for form validation
     const form = document.getElementsByTagName('form')[0];
+    const email = document.querySelector('#email');
+    const cardNumber = document.querySelector('#cc-num');
+    const zipCode = document.querySelector('#zip');
+    const cvv = document.querySelector('#cvv');
     
     /**
-     *  Helper function for checking "Name" field validity
+     *  Helper functions for checking each field validity
      *  @return {boolean} whether or not name field is blank
      */
     function nameIsValid() {
         return /[a-z]+/i.test(name.value);
     }
-    /**
-     *  Helper function for checking "Email Address"" validity
-     *  @return {boolean} whether or not the email is formatted properly 
-     */
     function emailIsValid() {
-        const email = document.querySelector('#email');
         return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email.value);
     }
-    /**
-     *  Helper function for checking "Register for Activities" validity
-     *  @return {boolean} whether or not any of the checkboxes in the section are checked 
-     */
     function activitiesIsValid() {
-        const checkboxes = activities.querySelectorAll('[type="checkbox"]');
         let selectionMade = false;
         for (let i=0; i<checkboxes.length; i++) {
             if (checkboxes[i].checked) {
@@ -129,115 +132,70 @@ window.addEventListener ('load', () => {
         }
         return selectionMade;
     }
-    /**
-     *  Helper function for checking "Payment Info" for credit card payments validity 
-     *      the variables have been pulled out for later access
-     * 
-     *  @return {boolean} whether or not correct number of digits have been entered in each field
-     */
-    const cardNumber = document.querySelector('#cc-num');
-    const cardIsValid = /^\d{13,16}$/.test(cardNumber.value);
-    const zipCode = document.querySelector('#zip');
-    const zipIsValid = /^\d{5}$/.test(zipCode.value)
-    const cvv = document.querySelector('#cvv');
-    const cvvIsValid = /^\d{3}$/.test(cvv.value);
+    function cardIsValid() {
+        return /^\d{13,16}$/.test(cardNumber.value);
+    }
+    function zipIsValid() {
+        return /^\d{5}$/.test(zipCode.value);
+    }
+    function cvvIsValid() {
+        return /^\d{3}$/.test(cvv.value);
+    }
     function payIsValid() {
-        console.log(cardNumber.value +' '+ zipCode.value +' '+ cvv.value);
-        console.log(cardIsValid +' '+ zipIsValid +' '+ cvvIsValid);
+        if (payMethod.value == 'select method') {
+            return false;
+        }
+        if (payMethod.value == 'paypal' || payMethod.value == 'bitcoin') {
+            return true;
+        }
         if (payMethod.value == 'credit-card') {
-            if (cardIsValid && zipIsValid && cvvIsValid) {
+            if (cardIsValid() && zipIsValid() && cvvIsValid()) {
                 return true;
             } else {
                 return false;
-            } 
-        } else if (payMethod.value == 'select method') {
-            return false;
-        } else {
-            return true;
+            }
         }
-    };
+    }
 
-// Error indicator helper functions
+// Error notifications for invalid form entries
     /**
      *  Helper function for notifying user of error in form element 
      *  @param {string} element - the form element that has invalid input
      */
-    function errorNotify(element) {
-        element.classList.add('not-valid');
-        element.classList.remove('valid');
-        element.lastElementChild.style.display = 'inherit';
+    function testForError(validityCheck, element) {
+        if (!validityCheck) {
+            element.classList.add('not-valid');
+            element.classList.remove('valid');
+            element.lastElementChild.style.display = 'inherit';
+        } else {
+            element.classList.remove('not-valid');
+            element.classList.add('valid');
+            element.lastElementChild.style.display = 'none';
+        }
     }
 
-    /**
-     *  Helper function for removing error notification from form element 
-     *  @param {string} element - the form element that has invalid input
-     */
-    function errorResolved(element) {
-        element.classList.remove('not-valid');
-        element.classList.add('valid');
-        element.lastElementChild.style.display = 'none';
-    }
-
-// Form submit event handler with form validation AND error notifications
+// Submit event handler with form validation AND error notifications
     form.addEventListener('submit', (e) => {
         if (nameIsValid() && emailIsValid() && activitiesIsValid() && payIsValid()) {
             // Submit form successfully
         } else {
             e.preventDefault();
 
-            if (!nameIsValid()) {
-                errorNotify(name.parentElement);
-            } else {
-                errorResolved(name.parentElement);
-            }
-            
-            if (!emailIsValid()) {
-                errorNotify(email.parentElement);
-            } else {
-                errorResolved(email.parentElement);
-            }
-            
-            if (!activitiesIsValid()) {
-                errorNotify(activities);
-            } else {
-                errorResolved(activities);
-            }
+            testForError(nameIsValid(), name.parentElement);
+            testForError(emailIsValid(), email.parentElement);
+            testForError(activitiesIsValid(), activities);
 
             if (!payIsValid()) {
+                if (payMethod.value == 'select method') {
+                    testForError(payIsValid(), payMethod.parentElement);
+                }
                 if (payMethod.value == 'credit-card') {
-                    //console.log(cardIsValid +' '+ zipIsValid +' '+ cvvIsValid);
-                    if (!cardIsValid) {
-                        errorNotify(cardNumber.parentElement);
-                    } else {
-                        errorResolved(cardNumber.parentElement);
-                    }
-                    if (!zipIsValid) {
-                        errorNotify(zipCode.parentElement);
-                    } else {
-                        errorResolved(zipCode.parentElement);
-                    }
-                    if (!cvvIsValid) {
-                        errorNotify(cvv.parentElement);
-                    } else {
-                        errorResolved(cvv.parentElement);
-                    }
-                } else if (payMethod.value == 'select method') {
-                    errorNotify(payMethod.parentElement);
-                } else {
-                    errorResolved(payMethod.parentElement);
+                    testForError(payIsValid(), payMethod.parentElement.parentElement.parentElement);
+                    testForError(cardIsValid(), cardNumber.parentElement);
+                    testForError(zipIsValid(), zipCode.parentElement);
+                    testForError(cvvIsValid(), cvv.parentElement);
                 }
             }
         }
     });
-
-// Accessibility - make focus states of "Activities" more obvious
-    const checkboxes = activities.querySelectorAll('[type="checkbox"]');
-    for (let i=0; i<checkboxes.length; i++) {
-        checkboxes[i].addEventListener('focus', (e) => {
-            checkboxes[i].parentElement.className = 'focus';
-        })
-        checkboxes[i].addEventListener('blur', (e) => {
-            checkboxes[i].parentElement.removeAttribute('class', 'focus')
-        })
-    }
 });
